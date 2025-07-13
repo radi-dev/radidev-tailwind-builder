2fbffc24e9512d561f3bd55177e073286edf5792
Radi <evaradi18@gmail.com>
2025-07-14 00:32:32 +0100

    fix: update CSS files for consistent styling and add new utility classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        general: "Raleway, ui-sans-serif, system-ui, sans-serif",
+      },
       colors: {
         primary: "#F0E0BC",
         secondary: "#28282B",
