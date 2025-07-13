0abcde7a8124f5efbe3aaf35de4c74b4ccab782e
Radi <evaradi18@gmail.com>
2025-07-13 18:17:31 +0100

    feat: initialize project with Express, Tailwind CSS, and custom styles
    - Added package.json with scripts for starting the server and watching Tailwind CSS
    - Created src/index.js to set up an Express server serving index.html
    - Added src/index.html with Tailwind CSS and custom styles
    - Introduced src/extra.css for additional styling
    - Created src/input.css for Tailwind CSS imports
    - Generated src/tailwind.css with Tailwind CSS styles
    - Configured tailwind.config.js for custom theme colors

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#F0E0BC",
+        secondary: "#28282B",
+        accent: "#BFDBDE",
+        dark: "#121212",
+        light: "#F8FAFC",
+      },
+    },
+  },
+  plugins: [],
+};
