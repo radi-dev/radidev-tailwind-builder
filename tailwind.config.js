c7130f8a1d776dc72d12a30b5ab19fc2d274c5ab
Radi <evaradi18@gmail.com>
2025-07-14 10:36:34 +0100

    fix: update font imports and classes in CSS; enhance layout and structure in index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        general: "Raleway, ui-sans-serif, system-ui, sans-serif",
+        1: "Raleway, ui-sans-serif, system-ui, sans-serif",
+        2: "Montserrat, ui-sans-serif, system-ui, sans-serif",
+        3: "Poppins, ui-sans-serif, system-ui, sans-serif",
       },
       colors: {
         primary: "#F0E0BC",
